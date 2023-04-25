4dfe9d8fcefdcdd5ca6df8458cce6bce69f84d8a
Misha <Misha140699@mail.ru>
2023-04-25 13:27:02 +0300

    this

---
+++ src/this.js
@@ -0,0 +1,29 @@
+// function getItem() {
+//   console.log(this);
+// }
+
+// getItem();
+
+// task 2
+// const item = {
+//     title: "Ball",
+//     getItem() {
+//         console.log({this});
+
+//     }
+// }
+
+// item.getItem();
+
+// task 3
+class Item {
+  title = "Ball";
+  getItem() {
+    function someFn() {
+      console.log("this", this);
+    }
+  }
+}
+
+const item = new Item();
+item.getItem();
