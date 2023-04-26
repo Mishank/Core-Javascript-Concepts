f80db884cc0f7654c1688af110930be5d44ee4ed
Misha <Misha140699@mail.ru>
2023-04-27 00:06:33 +0500

    feat/26-04 (#4)
    * change name
    
    * modules
    
    * implementDebounceFunction
    
    * implementThrottleFunction
    
    * working with DOM
    
    * addLink
    
    * splitEachSentencetoaSeparateLine
    
    * eventDelegation

---
+++ src/Core Javascript Concepts/Modules/common.js
@@ -0,0 +1,6 @@
+const getName = (name) => name;
+const getSurName = (surname) => surname;
+module.exports.getName = getName;
+module.exports.getSurName = getSurName;
+// module.exports.getFullname (name, surname) =>
+// `${getName(name)} $ {getSurName(surname)}`;  модификация устарела
