0a19ae8a8173177151176775c55f1f8d3d63149c
Misha <Misha140699@mail.ru>
2023-04-27 13:11:03 +0300

    parallelAsyncArray

---
+++ src/Asynchronous Javascript/parallelAsyncArray.js
@@ -0,0 +1,36 @@
+const asyncFunc1 = (callback) => {
+  setTimeout(() => {
+    callback(1);
+  }, 3000);
+};
+
+const asyncFunc2 = (callback) => {
+  setTimeout(() => {
+    callback(2);
+  }, 2000);
+};
+
+const asyncFunc3 = (callback) => {
+  setTimeout(() => {
+    callback(3);
+  }, 1000);
+};
+
+asyncParallel([asyncFunc1, asyncFunc2, asyncFunc3], (result) => {
+  console.log(result);
+});
+
+asyncParallel = (asyncFuncs, callback) => {
+  const resultArr = new Array(asyncFuncs.length);
+  const resultCounter = 0;
+
+  asyncFuncs.forEach((asyncFunc, index) => {
+    asyncFunc(() => {
+      resultArr[index] = value;
+      resultCounter++;
+      if (resultCounter >= asyncFunc.length) {
+        callback(resultArr);
+      }
+    });
+  });
+};
